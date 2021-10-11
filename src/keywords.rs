030b79cae54615f89caa450431d2f5c6618b94da
Marius Wilms <wilms.marius95@gmail.com>
2021-10-11 20:57:58 +0200

    First progress

---
+++ src/keywords.rs
@@ -0,0 +1,108 @@
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    Ident, LitStr, Result,
+};
+
+mod kw {
+    syn::custom_keyword!(describe);
+    syn::custom_keyword!(setup);
+    syn::custom_keyword!(test);
+    syn::custom_keyword!(teardown);
+}
+
+#[derive(Debug)]
+pub struct Describe {
+    name: String,
+    braces: syn::token::Brace,
+    setup: Option<Vec<Setup>>,
+    tests: Vec<Test>,
+    teardown: Option<Vec<Teardown>>,
+}
+
+impl syn::parse::Parse for Describe {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _describe = input.parse::<kw::describe>()?;
+        let name = input.parse::<syn::LitStr>()?.value();
+
+        let content;
+        let braces = syn::braced!(content in input);
+
+        let mut setup = Vec::<Setup>::new();
+        while !content.is_empty() {
+            setup.push(content.parse()?);
+        }
+
+        let mut tests = Vec::<Test>::new();
+
+        while !content.is_empty() {
+            eprintln!("{:#?}", content);
+            tests.push(content.parse()?);
+        }
+        //eprintln!("{:#?}", tests);
+
+        let mut teardown = Vec::<Teardown>::new();
+        while !content.is_empty() {
+            teardown.push(content.parse()?);
+        }
+
+        Ok(Self {
+            name,
+            braces,
+            setup: Some(setup),
+            tests,
+            teardown: Some(teardown),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Test {
+    name: String,
+    content: syn::Block,
+}
+
+impl syn::parse::Parse for Test {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _test = input.parse::<kw::test>()?;
+        let name = input.parse::<syn::LitStr>()?.value();
+        Ok(Self {
+            name,
+            content: input.parse::<syn::Block>()?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Setup {
+    name: String,
+    content: syn::Block,
+}
+
+impl syn::parse::Parse for Setup {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _test = input.parse::<kw::setup>()?;
+        let name = input.parse::<syn::LitStr>()?.value();
+        Ok(Self {
+            name,
+            content: input.parse::<syn::Block>()?,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Teardown {
+    name: String,
+    content: syn::Block,
+}
+
+impl syn::parse::Parse for Teardown {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let _test = input.parse::<kw::teardown>()?;
+        let name = input.parse::<syn::LitStr>()?.value();
+        Ok(Self {
+            name,
+            content: input.parse::<syn::Block>()?,
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,60 @@
 #![warn(clippy::all, clippy::pedantic)]
-
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
+
+use self::keywords::Describe;
+
+mod keywords;
 
 #[proc_macro]
 pub fn rs_unit(input: TokenStream) -> TokenStream {
-    input
+    let describe = parse_macro_input!(input as Describe);
+    //eprintln!("{:#?}", describe);
+
+    let expanded = quote! {
+        #[cfg(test)]
+        mod tests {
+
+        }
+    };
+
+    expanded.into()
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert!(true);
-    }
-}
+// use rs_unit::rs_unit;
+
+// fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
+
+// fn sub(a: i32, b: i32) -> i32 {
+//     a - b
+// }
+
+// fn main() {
+//     rs_unit! {
+
+//         describe "Test addition" {
+
+//             setup "Create database" {
+
+//             }
+
+//             test "success: Add positive numbers" {
+//                 let result = add(1,1);
+//                 assert_eq!(result, 2);
+//             }
+
+//             test "success: Add negative numbers" {
+//                 let result = add(-2, -2);
+//                 assert_eq!(result, -4);
+//             }
+
+//             teardown "Delete database" {
+
+//             }
+
+//         }
+//     }
+// }
